2d01270b6f4963a13d67619ebc40f21bc9e9aa7c
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-11-28 10:32:37 +0530

    use effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Basics from './Components/Basics';
+import LiveSearching from './Components/LiveSearching';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Basics /> */}
+      <LiveSearching />
     </div>
   );
 }
